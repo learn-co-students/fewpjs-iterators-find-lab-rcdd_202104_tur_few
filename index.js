3b1d0aa86b8ac53928bf10176fd8bb1bc6bf55b9
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-10 21:01:46 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "1969", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+const superbowlWin = (arr) => {
+  let win = arr.find(item => item.result === "W");
+  return win ? win.year : undefined;
+};
+superbowlWin(record)
