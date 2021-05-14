e74a2efa1cb579b3c9c34f3834627b593ceb0948
Hafise Nur Bacaksız <hafisenur.bacaksiz@gmail.com>
2021-05-14 18:15:06 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}]
+
+function superbowlWin(record){
+  let finding = record.find(result=> result.result==="W")
+  return finding ? finding.year : undefined;
+
+}
+superbowlWin(record);
