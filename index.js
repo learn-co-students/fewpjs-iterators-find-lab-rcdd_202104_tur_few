6fd2ed738d37aa6748fa045e500790af40cd7a8e
iseluin <kutay.kagan.ozenn@gmail.com>
2021-05-16 18:14:21 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin (arr) {
+  const result = arr.find(game => game.result === 'W');
+  let year;
+  if (result) year = result.year;
+  return year;
+}
